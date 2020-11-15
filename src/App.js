dd3a1c500a2b4793005bbf7782bc559098399b27
gladimir777 <gladimir777@github.com>
2020-11-15 17:59:50 -0300

    Adding notification component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DashboardNav from "./components/DashboardNav";
 import WelcomeCard from "./components/WelcomeCard";
 import HomeNav from "./components/HomeNav";
 import Hotels from "./components/Hotels";
+import Notification from "./components/Notification";
 
 const App = () => {
   return (
@@ -18,82 +19,7 @@ const App = () => {
               <HomeNav />
               <Hotels />
             </div>
-
-            <div className="sectionRight mb10">
-              <h2 className="ml10px textsize-1 fs-sbold cBlack">
-                Notificaciones
-              </h2>
-              <div className="notification mb30 mt10">
-                <div className="notif-left flex-display flexa-jcsb">
-                  {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
-                  <h4 className="textsize-3">02 abril</h4>
-                </div>
-                <div className="notif-right">
-                  <h4 className="mt4 textsize-2 fs-regular cBlue">
-                    Miguel Ruiz
-                  </h4>
-                  <p className="mt4 textsize-3 cGray">
-                    It is a long established fact that a reader will
-                  </p>
-                </div>
-              </div>
-              <div className="notification mb30">
-                <div className="notif-left flex-display flexa-jcsb">
-                  {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
-                  <h4 className="textsize-3">02 abril</h4>
-                </div>
-                <div className="notif-right">
-                  <h4 className="mt4 textsize-2 fs-regular cBlue">
-                    Miguel Ruiz
-                  </h4>
-                  <p className="mt4 textsize-3 cGray">
-                    It is a long established fact that a reader will
-                  </p>
-                </div>
-              </div>
-              <div className="notification mb30">
-                <div className="notif-left flex-display flexa-jcsb">
-                  {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
-                  <h4 className="textsize-3">02 abril</h4>
-                </div>
-                <div className="notif-right">
-                  <h4 className="mt4 textsize-2 fs-regular cBlue">
-                    Miguel Ruiz
-                  </h4>
-                  <p className="mt4 textsize-3 cGray">
-                    It is a long established fact that a reader will
-                  </p>
-                </div>
-              </div>
-              <div className="notification mb30">
-                <div className="notif-left flex-display flexa-jcsb">
-                  {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
-                  <h4 className="textsize-3">02 abril</h4>
-                </div>
-                <div className="notif-right">
-                  <h4 className="mt4 textsize-2 fs-regular cBlue">
-                    Miguel Ruiz
-                  </h4>
-                  <p className="mt4 textsize-3 cGray">
-                    It is a long established fact that a reader will
-                  </p>
-                </div>
-              </div>
-              <div className="notification mb30">
-                <div className="notif-left flex-display flexa-jcsb">
-                  {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
-                  <h4 className="textsize-3">02 abril</h4>
-                </div>
-                <div className="notif-right">
-                  <h4 className="mt4 textsize-2 fs-regular cBlue">
-                    Miguel Ruiz
-                  </h4>
-                  <p className="mt4 textsize-3 cGray">
-                    It is a long established fact that a reader will
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Notification />
           </div>
         </section>
       </div>
---
+++ src/components/Notification.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Notification = () => {
+  return (
+    <>
+      <div className="sectionRight mb10">
+        <h2 className="ml10px textsize-1 fs-sbold cBlack">Notificaciones</h2>
+        <div className="notification mb30 mt10">
+          <div className="notif-left flex-display flexa-jcsb">
+            {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
+            <h4 className="textsize-3">02 abril</h4>
+          </div>
+          <div className="notif-right">
+            <h4 className="mt4 textsize-2 fs-regular cBlue">Miguel Ruiz</h4>
+            <p className="mt4 textsize-3 cGray">
+              It is a long established fact that a reader will
+            </p>
+          </div>
+        </div>
+        <div className="notification mb30">
+          <div className="notif-left flex-display flexa-jcsb">
+            {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
+            <h4 className="textsize-3">02 abril</h4>
+          </div>
+          <div className="notif-right">
+            <h4 className="mt4 textsize-2 fs-regular cBlue">Miguel Ruiz</h4>
+            <p className="mt4 textsize-3 cGray">
+              It is a long established fact that a reader will
+            </p>
+          </div>
+        </div>
+        <div className="notification mb30">
+          <div className="notif-left flex-display flexa-jcsb">
+            {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
+            <h4 className="textsize-3">02 abril</h4>
+          </div>
+          <div className="notif-right">
+            <h4 className="mt4 textsize-2 fs-regular cBlue">Miguel Ruiz</h4>
+            <p className="mt4 textsize-3 cGray">
+              It is a long established fact that a reader will
+            </p>
+          </div>
+        </div>
+        <div className="notification mb30">
+          <div className="notif-left flex-display flexa-jcsb">
+            {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
+            <h4 className="textsize-3">02 abril</h4>
+          </div>
+          <div className="notif-right">
+            <h4 className="mt4 textsize-2 fs-regular cBlue">Miguel Ruiz</h4>
+            <p className="mt4 textsize-3 cGray">
+              It is a long established fact that a reader will
+            </p>
+          </div>
+        </div>
+        <div className="notification mb30">
+          <div className="notif-left flex-display flexa-jcsb">
+            {/* <ion-icon className="icoGray" name="mail-open-outline"></ion-icon> */}
+            <h4 className="textsize-3">02 abril</h4>
+          </div>
+          <div className="notif-right">
+            <h4 className="mt4 textsize-2 fs-regular cBlue">Miguel Ruiz</h4>
+            <p className="mt4 textsize-3 cGray">
+              It is a long established fact that a reader will
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Notification;
